79afc13881a40f442fff6d745d98440814a15011
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-18 14:27:15 +0200

    Add project files.

---
+++ 20. Static Attributes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Static_Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs song1 = new Songs("Holiday","John",115);
+            Songs song2 = new Songs("Billy Eish", "Goodnight", 155);
+            Console.WriteLine(song1.title);
+            Console.WriteLine(Songs.songCount);
+
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 20. Static Attributes/Songs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20.Static_Attributes
+{
+    internal class Songs
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        //declaring a static attribute
+        public static int songCount;
+        //constructor
+        public Songs(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            songCount++;
+        }
+    }
+}
